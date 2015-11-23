03b5b93b2530b99a4cf1401f376c1f61b9cc8511
Amit Dubey <amit.dubey2219@gmail.com>
2015-11-23 00:20:32 -0800

    adding APIs for browse by category and product

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+//set-up connection to mongodb
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/cmpe226catalog');
+
+//import mongoose models
+require('./model/catalogModel');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog');
 
+//init express app
 var app = express();
 
 // view engine setup
@@ -23,6 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/browse', catalog)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -31,7 +40,6 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handlers
 
 // development error handler
 // will print stacktrace
---
+++ routes/catalog.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+
+var catalog = mongoose.model('productcatalogs');
+
+/* Remove when testing is complete */
+router.get('/', function(req, res, next) {
+  res.json("hello world from catalog");
+});
+
+
+router.get('/category/:category', function(req, res, next) {
+	catalog.find({ "category" : req.param('category')}, {'productName' :1 , 'shortDesc': 1 , 'price' : 1} ,
+		function(err, data){
+				if (err) { return next(err); }
+	   			if (!data) { return next(new Error("can't find products in category")); }
+			   	
+			    res.json(data);
+			});
+});
+
+router.get('/product/:productId', function(req, res, next) {
+	catalog.findById(req.param('productId'), 
+		function(err, data) {
+			if (err) { return next(err); }
+	   			if (!data) { return next(new Error("can't find product")); }
+			   
+			    res.json(data);
+		});
+});
+
+module.exports = router;
