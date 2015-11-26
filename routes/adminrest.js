4baabe6dd128cd64d3c2d312c246b91a5fae1ab9
pvr17 <prashant.rohilla>
2015-11-25 16:10:39 -0800

    added admin services

---
+++ routes/adminrest.js
@@ -0,0 +1,60 @@
+var mysql = require("mysql");
+var express = require('express');
+var app = express.Router();
+
+connection = mysql.createPool({
+        host     : 'cmpe226.cpodi2zqj9hl.us-east-1.rds.amazonaws.com',
+        user     : 'cmpe226',
+        password : 'project226',
+        database : 'cmpe226',
+        multipleStatements: true
+    });
+
+var data = {
+        "error":1
+    };
+
+app.get('/admin', function(req,res) {
+  res.json("hello world from catalog");
+});
+
+app.get('/admin/orders', function(req,res){ 
+    connection.query("SELECT orders.OrderId, OrderDate, ShippingDate,
+     OrderStatus,ProductId, TotalPrice, Quantity from orders, 
+        orderdetails where orders.OrderId = orderdetails.OrderId;", function (err,result) {  
+        if(err){
+            throw err;
+        }
+        else{
+        res.json(result);
+        }                    
+    });
+});
+
+app.get('/admin/customers', function(req,res){ 
+    connection.query("select * from customers", function (err,result) {  
+         if(err){
+            throw err;
+        }
+        else{
+        res.json(result);
+        }                  
+    });
+});
+
+app.get('/admin/products', function(req,res){
+	connection.query("select * from products", function (err,result) {  
+         if(err){
+            throw err;
+        }
+        else{
+        res.json(result);
+        }                    
+    });
+});
+
+var resdata={
+    "response":1
+}
+
+module.exports = app;
