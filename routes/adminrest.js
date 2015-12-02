a69d44ab9aba625d192ab17d04d6d6d685de80ec
pvr17 <prashant.rohilla>
2015-12-01 16:21:12 -0800

    drill and pivot

--- routes/adminrest.js
+++ routes/adminrest.js
@@ -103,4 +103,97 @@ var resdata={
     "response":1
 }
 
+app.get('/admin/salesdrill', function(req,res){
+
+    var verb=req.body.drill;
+
+    if(verb=="normal"){
+        connection.query("SELECT YEAR(SaleDate) AS year, QUARTER(SaleDate) AS quarter, SUM(UnitsSold)
+                                AS unitsSold
+                                    FROM sales_fact_table
+                                        GROUP BY YEAR(SaleDate), QUARTER(SaleDate)", function (err,result) {  
+         if(err){
+            throw err;
+        }
+        else{
+        res.json(result);
+        }                    
+    });
+    }
+    else if(verb=="up"){
+        connection.query("SELECT YEAR(SaleDate) AS year,  SUM(UnitsSold) AS unitsSold FROM sales_fact_table 
+                                GROUP BY YEAR(SaleDate)", function (err,result) {  
+         if(err){
+            throw err;
+        }
+        else{
+        res.json(result);
+        }                    
+    });
+    }
+    else if(verb=="down"){
+        connection.query("SELECT YEAR(SaleDate) AS year, MONTH(SaleDate) AS Month, SUM(UnitsSold)
+                            AS unitsSold
+                                FROM sales_fact_table
+                                    GROUP BY YEAR(SaleDate), MONTH(SaleDate)", function (err,result) {  
+         if(err){
+            throw err;
+        }
+        else{
+        res.json(result);
+        }                    
+    });
+    }
+    
+});
+
+app.get('/admin/pivot', function(req,res){
+
+        var verb=req.body.pivot;
+
+        if(verb=="normal"){
+
+        connection.query("Select year(SaleDate) as year, 
+                            MONTHNAME(SaleDate) as month, 
+                                sum(UnitsSold) as TotalSold 
+                                    from sales_fact_table group by year(SaleDate), 
+                                        Month(SaleDate);", function (err,result) {  
+             if(err){
+                throw err;
+                }
+            else{
+                res.json(result);
+                }                    
+        });
+        }
+        else if(verb=="pivoted"){
+                connection.query("SELECT Year, Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, `Dec`
+                        FROM ( SELECT year(SaleDate) AS 'Year', 
+                            Sum(If( Month(SaleDate)= 1, UnitsSold, 0 )) AS Jan, 
+                            Sum(If( Month(SaleDate)= 2, UnitsSold, 0 )) AS Feb, 
+                            Sum(If( Month(SaleDate)= 3, UnitsSold, 0 )) AS Mar, 
+                            Sum(If( Month(SaleDate)= 4, UnitsSold, 0 )) AS Apr, 
+                            Sum(If( Month(SaleDate)= 5, UnitsSold, 0 )) AS May, 
+                            Sum(If( Month(SaleDate)= 6, UnitsSold, 0 )) AS Jun, 
+                            Sum(If( Month(SaleDate)= 7, UnitsSold, 0 )) AS Jul, 
+                            Sum(If( Month(SaleDate)= 8, UnitsSold, 0 )) AS Aug, 
+                            Sum(If( Month(SaleDate)= 9, UnitsSold, 0 )) AS Sep, 
+                            Sum(If( Month(SaleDate)=10, UnitsSold, 0 )) AS Oct, 
+                            Sum(If( Month(SaleDate)=11, UnitsSold, 0 )) AS Nov, 
+                            Sum(If( Month(SaleDate)=12, UnitsSold, 0 )) AS `Dec`
+                          FROM sales_fact_table
+                          GROUP BY year  
+                        ) AS sums ;", function (err,result) {  
+                                     if(err){
+                                        throw err;
+                                        }
+                                    else{
+                                        res.json(result);
+                                        }                    
+                                });
+                                }
+
+    
+        });
+
 module.exports = app;
